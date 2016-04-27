e0e537189501ce4883d9b8a63e3d1ab8d8b56763
Andrew Ek <andrewek@gmail.com>
2016-04-26 23:20:45 -0500

    Full HTML, stubs for CSS and JS

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+
+  // initialize whatever variables you need and add your event listeners
+  // you can (but probably shouldn't) define functions inside this function
+
+};
