497c6ff2d2d0e31da0a037cdd5cfb4f2ea82ea59
Patrick <patrickwiechert@yahoo.com>
2016-04-28 09:04:55 -0500

    good times

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-window.addEventListener("load", setUpPage);
+window.addEventListener("load", function() {
 
-function setUpPage () {
-
-  // initialize whatever variables you need and add your event listeners
-  // you can (but probably shouldn't) define functions inside this function
-
-};
+var squares = document.getElementsByClassName("board-tile");
 
 function changeSquare() {
-  var square = document.getElementById("board-tile");
-  square.innerHTML("X");
-  title.setAttribute("style", "color: blue");
+  this.innerHTML="X";
 }
+
+for(var i = 0; i < squares.length; i++){
+  squares[i].addEventListener("click", changeSquare);
+}
+
+
+
+});
