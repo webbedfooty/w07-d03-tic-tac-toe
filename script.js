632abde983a0e86ecc5166715479c0aac6b87ec4
Patrick <patrickwiechert@yahoo.com>
2016-04-29 11:24:02 -0500

    fixed player turn message

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var squares = document.getElementsByClassName("board-tile");
 for(var i = 0; i < squares.length; i++){
   squares[i].addEventListener("click", changeSquare);
   squares[i].addEventListener("click", changePlayerTurn);
+
 }
 
 });
@@ -11,11 +12,22 @@ var playerTurn = "X";
 
   function changePlayerTurn() {
     if (playerTurn === "X"){
+      elementX = document.getElementById("player-x-ready");
+      elementX.setAttribute("class", "hidden");
+      elementO = document.getElementById("player-o-ready");
+      elementO.setAttribute("class", "visible");
+      this.removeEventListener("click", changePlayerTurn);
       playerTurn = "O";
     }else {
+      elementO = document.getElementById("player-o-ready");
+      elementO.setAttribute("class", "hidden");
+      elementX = document.getElementById("player-x-ready");
+      elementX.setAttribute("class", "visible");
+      this.removeEventListener("click", changePlayerTurn);
       playerTurn = "X";
-    };
+    }
   }
+
 
   function changeSquare() {
     if (playerTurn === "X"){
