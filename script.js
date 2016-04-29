4229e0622eb9f1030906d660a2283df6414ad4fd
Patrick <patrickwiechert@yahoo.com>
2016-04-29 10:04:09 -0500

    got the xs and os to mark and not change  yeahhhhh

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 window.addEventListener("load", function() {
 
 var squares = document.getElementsByClassName("board-tile");
-
-function changeSquare() {
-  this.innerHTML="X";
-}
-
 for(var i = 0; i < squares.length; i++){
   squares[i].addEventListener("click", changeSquare);
+  squares[i].addEventListener("click", changePlayerTurn);
 }
 
-
-
 });
+var playerTurn = "X";
+
+  function changePlayerTurn() {
+    if (playerTurn === "X"){
+      playerTurn = "O";
+    }else {
+      playerTurn = "X";
+    };
+  }
+
+  function changeSquare() {
+    if (playerTurn === "X"){
+        this.innerHTML="X";
+        this.removeEventListener("click", changeSquare);
+
+      }else {
+        this.innerHTML="O";
+        this.removeEventListener("click", changeSquare);
+    }
+  }
