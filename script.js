a75e98ea4c64cf8fd1d88d0d72d87745197ad791
Patrick <patrickwiechert@yahoo.com>
2016-05-03 15:50:56 -0500

    minor tweaks

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ window.addEventListener("load", playGame);
 
 function playGame() {
 
+  var newStart = document.getElementsByClassName("new-game");
+  for(var i = 0; i < newStart.length; i++){
+    newStart[i].addEventListener("click", newGame);
+  }
+
+  var reset = document.getElementById("reset-button");
+  for(var i = 0; i < reset.length; i++){
+    reset[i].addEventListener("click", reset);
+  }
+
   var squares = document.getElementsByClassName("board-tile");
 // sets EventListeners on all 9 squares
   for(var i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", changeSquare);
     squares[i].addEventListener("click", changePlayerTurn);
+    squares[i].innerHTML="";
   }
 // starting variables
   var playerTurn = "X";
@@ -16,6 +27,37 @@ function playGame() {
   var oScore = 0;
   var cScore = 0;
   var winner;
+
+  function reset() {
+    var squares = document.getElementsByClassName("board-tile");
+    for(var i = 0; i < squares.length; i++){
+    squares[i].addEventListener("click", changeSquare);
+    squares[i].addEventListener("click", changePlayerTurn);
+    squares[i].innerHTML="";
+    }
+    var scores = document.getElementsByClassName("score");
+    for(var i = 0; i < scores.length; i++){
+    scores[i].innerHTML="";
+    }
+    var gameLog = document.getElementsByClassName("game-log-list");
+    for(var i = 0; i < gameLog.length; i++){
+    gameLog[i].innerHTML="";
+    }
+
+    var result = document.getElementsByClassName("outcome-message");
+    for(var i = 0; i < result.length; i++){
+    result[i].setAttribute("class", "hidden");
+    }
+
+    var moveCounter = 0;
+    var totalMoves = 9;
+    var xScore = 0;
+    var oScore = 0;
+    var cScore = 0;
+    var winner;
+    playGame();
+
+  }
 
   function changePlayerTurn() {
     if(playerTurn === "X"){
@@ -59,136 +101,114 @@ function playGame() {
     }
   }
 
-function checkWinner(){
+  function checkWinner(){
 // assigns id tags to variables for future analysis
-  var tile11=document.getElementById("tile-1-1").innerHTML;
-  var tile12=document.getElementById("tile-1-2").innerHTML;
-  var tile13=document.getElementById("tile-1-3").innerHTML;
-  var tile21=document.getElementById("tile-2-1").innerHTML;
-  var tile22=document.getElementById("tile-2-2").innerHTML;
-  var tile23=document.getElementById("tile-2-3").innerHTML;
-  var tile31=document.getElementById("tile-3-1").innerHTML;
-  var tile32=document.getElementById("tile-3-2").innerHTML;
-  var tile33=document.getElementById("tile-3-3").innerHTML;
+    var tile11=document.getElementById("tile-1-1").innerHTML;
+    var tile12=document.getElementById("tile-1-2").innerHTML;
+    var tile13=document.getElementById("tile-1-3").innerHTML;
+    var tile21=document.getElementById("tile-2-1").innerHTML;
+    var tile22=document.getElementById("tile-2-2").innerHTML;
+    var tile23=document.getElementById("tile-2-3").innerHTML;
+    var tile31=document.getElementById("tile-3-1").innerHTML;
+    var tile32=document.getElementById("tile-3-2").innerHTML;
+    var tile33=document.getElementById("tile-3-3").innerHTML;
 // examines the 8 winning combinations to see if X has won
-  if((tile11 === "X" && tile12 === "X" && tile13 === "X") ||
-     (tile21 === "X" && tile22 === "X" && tile23 === "X") ||
-     (tile31 === "X" && tile32 === "X" && tile33 === "X") ||
-     (tile11 === "X" && tile21 === "X" && tile31 === "X") ||
-     (tile12 === "X" && tile22 === "X" && tile32 === "X") ||
-     (tile13 === "X" && tile23 === "X" && tile33 === "X") ||
-     (tile11 === "X" && tile22 === "X" && tile33 === "X") ||
-     (tile31 === "X" && tile22 === "X" && tile13 === "X")){
+    if((tile11 === "X" && tile12 === "X" && tile13 === "X") ||
+       (tile21 === "X" && tile22 === "X" && tile23 === "X") ||
+       (tile31 === "X" && tile32 === "X" && tile33 === "X") ||
+       (tile11 === "X" && tile21 === "X" && tile31 === "X") ||
+       (tile12 === "X" && tile22 === "X" && tile32 === "X") ||
+       (tile13 === "X" && tile23 === "X" && tile33 === "X") ||
+       (tile11 === "X" && tile22 === "X" && tile33 === "X") ||
+       (tile31 === "X" && tile22 === "X" && tile13 === "X")){
 // makes X winner message visible and hides player turn message
-    elX = document.getElementById("player-x-wins");
-    elX.setAttribute("class", "visible");
-    elementX = document.getElementById("player-x-ready");
-    elementX.setAttribute("class", "hidden");
+      elX = document.getElementById("player-x-wins");
+      elX.setAttribute("class", "visible");
+      elementX = document.getElementById("player-x-ready");
+      elementX.setAttribute("class", "hidden");
 // increments X score and updates the visible html code
-    xScore++;
-    scoreX = document.getElementById("x-score");
-    scoreX.innerHTML=xScore;
+      xScore++;
+      scoreX = document.getElementById("x-score");
+      scoreX.innerHTML=xScore;
 //    alert("PLAYER X WINS!");
 // launches endGame process
-    endGame();
+      endGame();
 // adds an entry to the running Game Log
-    var para = document.createElement("li");
-    var node = document.createTextNode("PLAYER X WINS!");
-    para.appendChild(node);
-    var element = document.getElementById("game-log");
-    element.appendChild(para);
+      var para = document.createElement("li");
+      var node = document.createTextNode("PLAYER X WINS!");
+      para.appendChild(node);
+      var element = document.getElementById("game-log");
+      element.appendChild(para);
 // examines the 8 winning combinations to see if O has won
-  }else if((tile11 === "O" && tile12 === "O" && tile13 === "O") ||
-    (tile21 === "O" && tile22 === "O" && tile23 === "O") ||
-    (tile31 === "O" && tile32 === "O" && tile33 === "O") ||
-    (tile11 === "O" && tile21 === "O" && tile31 === "O") ||
-    (tile12 === "O" && tile22 === "O" && tile32 === "O") ||
-    (tile13 === "O" && tile23 === "O" && tile33 === "O") ||
-    (tile11 === "O" && tile22 === "O" && tile33 === "O") ||
-    (tile31 === "O" && tile22 === "O" && tile13 === "O")){
+    }else if((tile11 === "O" && tile12 === "O" && tile13 === "O") ||
+      (tile21 === "O" && tile22 === "O" && tile23 === "O") ||
+      (tile31 === "O" && tile32 === "O" && tile33 === "O") ||
+      (tile11 === "O" && tile21 === "O" && tile31 === "O") ||
+      (tile12 === "O" && tile22 === "O" && tile32 === "O") ||
+      (tile13 === "O" && tile23 === "O" && tile33 === "O") ||
+      (tile11 === "O" && tile22 === "O" && tile33 === "O") ||
+      (tile31 === "O" && tile22 === "O" && tile13 === "O")){
 // makes O winner message visible and hides player turn message
-    elO = document.getElementById("player-o-wins");
-    elO.setAttribute("class", "visible");
-    elementO = document.getElementById("player-o-ready");
-    elementO.setAttribute("class", "hidden");
+      elO = document.getElementById("player-o-wins");
+      elO.setAttribute("class", "visible");
+      elementO = document.getElementById("player-o-ready");
+      elementO.setAttribute("class", "hidden");
 // increments O score and updates the visible html code
-    oScore++;
-    scoreO = document.getElementById("o-score");
-    scoreO.innerHTML=oScore;
+      oScore++;
+      scoreO = document.getElementById("o-score");
+      scoreO.innerHTML=oScore;
 //    alert("PLAYER O WINS!");
 // launches endGame process
-    endGame();
+      endGame();
 // adds an entry to the running Game Log
-    var para = document.createElement("li");
-    var node = document.createTextNode("PLAYER O WINS!");
-    para.appendChild(node);
-    var element = document.getElementById("game-log");
-    element.appendChild(para);
+      var para = document.createElement("li");
+      var node = document.createTextNode("PLAYER O WINS!");
+      para.appendChild(node);
+      var element = document.getElementById("game-log");
+      element.appendChild(para);
 // if a 9 moves have been made and no winner is declared, it is a tie
-  }else if((moveCounter === 9) && (winner === undefined)){
+    }else if((moveCounter === 9) && (winner === undefined)){
 // makes Tie game message visible
-    elC = document.getElementById("tie-game");
-    elC.setAttribute("class", "visible");
+      elC = document.getElementById("tie-game");
+      elC.setAttribute("class", "visible");
 // increments Cat score and updates the visible html code
-    cScore++;
-    scoreC = document.getElementById("c-score");
-    scoreC.innerHTML=cScore;
+      cScore++;
+      scoreC = document.getElementById("c-score");
+      scoreC.innerHTML=cScore;
 //    alert("STALEMATE");
 // launches endGame process
-    endGame();
+      endGame();
 // adds an entry to the running Game Log
-    var para = document.createElement("li");
-    var node = document.createTextNode("STALEMATE.");
-    para.appendChild(node);
-    var element = document.getElementById("game-log");
-    element.appendChild(para);
-  }
-}
-// endGame procedure to hide messages and turn off all EventListeners
-function endGame(){
-  elementX = document.getElementById("player-x-ready");
-  elementX.setAttribute("class", "hidden");
-  elementO = document.getElementById("player-o-ready");
-  elementO.setAttribute("class", "hidden");
-    for(var i = 0; i < squares.length; i++){
-      squares[i].removeEventListener("click", changeSquare);
-      squares[i].removeEventListener("click", changePlayerTurn);
+      var para = document.createElement("li");
+      var node = document.createTextNode("STALEMATE.");
+      para.appendChild(node);
+      var element = document.getElementById("game-log");
+      element.appendChild(para);
     }
-}
+  }
+// endGame procedure to hide messages and turn off all EventListeners
+  function endGame(){
+    elementX = document.getElementById("player-x-ready");
+    elementX.setAttribute("class", "hidden");
+    elementO = document.getElementById("player-o-ready");
+    elementO.setAttribute("class", "hidden");
+      for(var i = 0; i < squares.length; i++){
+        squares[i].removeEventListener("click", changeSquare);
+        squares[i].removeEventListener("click", changePlayerTurn);
+      }
 
-var newStart = document.getElementsByClassName("new-game");
-for(var i = 0; i < newGame.length; i++){
-  newStart[i].addEventListener("click", newGame);
-};
+  }
 
-var reset = document.getElementById("reset-button");
-for(var i = 0; i < newGame.length; i++){
-  reset[i].addEventListener("click", reset);
-};
-
-}
-function newGame(){
-//   var squares = document.getElementsByClassName("board-tile");
-// // sets EventListeners on all 9 squares
-//   for(var i = 0; i < squares.length; i++){
-//     squares[i].addEventListener("click", changeSquare);
-//     squares[i].addEventListener("click", changePlayerTurn);
-//   }
+  function newGame(){
+    var result = document.getElementsByClassName("outcome-message");
+    for(var i = 0; i < result.length; i++){
+    result[i].setAttribute("class", "hidden");
+    }
+// preps the next game with change in player starting the game
+    changePlayerTurn();
 // starting variables
-  var moveCounter = 0;
-  var totalMoves = 9;
-  var winner;
-  playGame();
-}
-
-function reset() {
-  var playerTurn = "X";
-  var moveCounter = 0;
-  var totalMoves = 9;
-  var xScore = 0;
-  var oScore = 0;
-  var cScore = 0;
-  var winner;
-  playGame();
-
-}
+    var moveCounter = 0;
+    var totalMoves = 9;
+    playGame();
+  }
+};
